c28e3f02dd7bc1d566b24e1ff7ab40079ac61ac5
Dale Xu <69087617+Dale-Xu1@users.noreply.github.com>
2020-11-23 15:36:25 -0500

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, FormEvent } from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Props { }
+
+interface State
+{
+    equation: string
+}
+
+class App extends React.Component<Props>
+{
+
+    public state: State =
+    {
+        equation: ""
+    }
+
+
+    public constructor(props: Props)
+    {
+        super(props)
+        
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+
+    private handleSubmit(e: FormEvent)
+    {
+        e.preventDefault()
+        console.log(this.state.equation);
+    }
+
+    private handleChange(e: ChangeEvent<HTMLInputElement>)
+    {
+        this.setState({ equation: e.target.value });
+    }
+
+    public render(): React.ReactElement
+    {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.equation} onChange={this.handleChange} />
+                    <input type="submit" value="Generate" />
+                </form>
+            </div>
+        )
+    }
+
 }
 
 export default App;
